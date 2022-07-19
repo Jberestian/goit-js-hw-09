99182c8c4b719fbcdcf624293df31adf6c5a5454
Jberestian <31864132+Jberestian@users.noreply.github.com>
2022-07-19 12:41:01 +0000

    Deploying to gh-pages from @ Jberestian/goit-js-hw-09@55e5c839f61830918f0a1a8f4c594e90964a6fe8 🚀

---
+++ 01-color-switcher.921d32cc.js
@@ -0,0 +1,2 @@
+const t={startBtnEl:document.querySelector("button[data-start]"),stopBtnEl:document.querySelector("button[data-stop]"),body:document.querySelector("body")};let e=null;t.stopBtnEl.disabled=!0,t.startBtnEl.addEventListener("click",(()=>{t.startBtnEl.disabled=!0,t.stopBtnEl.disabled=!1,e=setInterval((()=>{t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),t.stopBtnEl.addEventListener("click",(()=>{clearInterval(e),t.startBtnEl.disabled=!1,t.stopBtnEl.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.921d32cc.js.map
